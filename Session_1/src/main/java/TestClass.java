7909f6007089c689dd17a541ce06b21b756beaae
Sergei Voitenko <svoitenko@lohika.com>
2015-10-05 15:49:50 +0300

    Add cycle - commit2

--- Session_1/src/main/java/TestClass.java
+++ Session_1/src/main/java/TestClass.java
@@ -6,6 +6,12 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+
+
+
         int i=0;
         while (i<10){
             System.out.println("i = " + i);
