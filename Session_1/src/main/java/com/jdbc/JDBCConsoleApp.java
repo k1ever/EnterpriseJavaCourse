04b3f6f1e3195ce4ace35c58459263f9304bd356
Sergei Voitenko <voitenko.sv@gmail.com>
2015-10-14 21:06:15 +0300

    Added connection init code

---
+++ Session_1/src/main/java/com/jdbc/JDBCConsoleApp.java
@@ -0,0 +1,36 @@
+package com.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by Klever on 14.10.15.
+ */
+public class JDBCConsoleApp {
+
+    public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+        JDBCConsoleApp jdbcConsoleApp = new JDBCConsoleApp();
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Properties properties = new Properties();
+        jdbcConsoleApp.loadProperties(properties);
+
+        String url = properties.getProperty("url");
+        String userName = properties.getProperty("user.name");
+        String password = properties.getProperty("user.password");
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        System.out.println(connection.getCatalog());
+    }
+
+    public void loadProperties(Properties properties) throws IOException {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("jdbc.config.files/jdbc.properties");
+        properties.load(in);
+    }
+
+
+}
